1dc8292821cdf410c59d54f608ddcb1215656e9e
Bryan Hu <bryan.hu.2020@gmail.com>
2021-04-05 12:12:22 -0700

    :sparkles: Verbose logging
    Resolves #7

--- slashtilities/__main__.py
+++ slashtilities/__main__.py
@@ -32,7 +32,9 @@ def get_testing_guilds():
     guild_ids=get_testing_guilds(),
 )
 async def ping(ctx):
-    await ctx.send(f":ping_pong: Pong! | (`{client.latency*1000:.4}` ms)")
+    gotten_ping = client.latency * 1000
+    print(f"Recorded ping: {gotten_ping} ms")
+    await ctx.send(f":ping_pong: Pong! | (`{gotten_ping:.4}` ms)")
 
 
 @slash.slash(
@@ -49,26 +51,33 @@ async def igotpinged(ctx):
             ":x: How do you expect me to find your last message if "
             "I don't even have access to this channel???"
         )
-        return
-    async for message in ctx.channel.history(
-        after=last_msg,
-        limit=None,  # or 9000?
-    ):
-        if ctx.author.mentioned_in(message):
-            await ctx.send(
-                f":mag: {message.author} did, right here: {message.jump_url}"
-            )
-            break
     else:
-        await ctx.send(
-            ":ghost: I didn't find anyone. You probably got ***ghost pinged***"
-        )
+        print("Getting last ping...", end=" ")
+        async for message in ctx.channel.history(
+            after=last_msg,
+            limit=None,  # or 9000?
+        ):
+            if ctx.author.mentioned_in(message):
+                print("\N{WHITE HEAVY CHECK MARK}")
+                await ctx.send(
+                    f":mag: {message.author} did, right here: {message.jump_url}"
+                )
+                break
+        else:
+            print("\N{GHOST}")
+            await ctx.send(
+                ":ghost: I didn't find anyone. You probably got ***ghost pinged***"
+            )
+    print("END OF `igotpinged`")
 
 
 async def get_last_message_from(author, channel):
+    print("Getting last message...", end=" ")
     async for message in channel.history(oldest_first=False, limit=None):
         if message.author.id == author.id:
+            print("\N{WHITE HEAVY CHECK MARK} found")
             return message
+    print("\N{CROSS MARK} Not found")
     return None
 
 
