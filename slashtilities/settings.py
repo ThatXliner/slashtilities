dbd92af378a9e98729c39e9214f9dd71d802f659
ThatXliner <bryan.hu.2020@gmail.com>
2021-06-16 10:32:42 -0700

    :bug: Handle TypeErrors for database

--- slashtilities/settings.py
+++ slashtilities/settings.py
@@ -1,3 +1,4 @@
+import asyncpg
 import discord
 from discord.ext import commands
 from slashtilities import db, utils
@@ -67,7 +68,8 @@ class Settings(commands.Cog):
         await ctx.send(embed=to_send)
 
     async def settings_set(self, ctx, setting_name: str, to: str) -> None:
-        if setting_name.lower() not in self.SETTING_NAMES:
+        setting_name = setting_name.lower()
+        if setting_name not in self.SETTING_NAMES:
             await ctx.send(embed=await utils.errorize("Setting not found"))
         else:
             try:
@@ -75,9 +77,13 @@ class Settings(commands.Cog):
             except ValueError:
                 await ctx.send(embed=await utils.errorize(f"{to} is an invalid value"))
             else:
-                await db.update_preferences_for(ctx.author.id, converted)
-                await ctx.send(
-                    embed=await utils.success(
-                        f"The setting `{setting_name.lower()}` is now `{converted}`"
+                try:
+                    await db.update_preferences_for(ctx.author.id, converted)
+                except asyncpg.exceptions.DataError:
+                    await ctx.send(embed=await utils.errorize(f"{to} is an invalid value for {setting_name}"))
+                else:
+                    await ctx.send(
+                        embed=await utils.success(
+                            f"The setting `{setting_name}` is now `{converted}`"
+                        )
                     )
-                )
