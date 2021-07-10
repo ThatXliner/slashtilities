1d21418d3eb77bed8fb68303aea0245c6670b3b0
Bryan Hu <bryan.hu.2020@gmail.com>
2021-07-10 15:15:18 -0700

    :bug: Fixed autoselfhost.py to work

--- autoselfhost.py
+++ autoselfhost.py
@@ -3,7 +3,7 @@ import shutil
 import sys
 import ensurepip
 import platform
-from typing import NoReturn, List
+from typing import NoReturn
 import re
 from pathlib import Path
 
@@ -98,8 +98,8 @@ if not_found:
         "Some requirements where not found\n"
         + "\n".join((f"{thing}: {how}" for thing, how in not_found.items()))
     )
-
-print(" Done ".center(shutil.get_terminal_size().columns, "="))
+print()
+print("=" * shutil.get_terminal_size().columns)
 
 print("Resolving pip", end="...", flush=True)
 USE_PIP = False
@@ -114,6 +114,8 @@ info("Cloning slashtilities")
 subprocess.run(
     [found["git"], "clone", "https://github.com/ThatXliner/slashtilities.git"],
     check=True,
+    stdout=subprocess.DEVNULL,
+    stderr=subprocess.STDOUT,
 )
 SLASH_PATH = str(Path(".").resolve().joinpath("slashtilities"))
 if USE_PIP:
@@ -122,6 +124,8 @@ if USE_PIP:
         [found["python"], "-m", "venv", ".venv"],
         check=True,
         cwd=SLASH_PATH,
+        stdout=subprocess.DEVNULL,
+        stderr=subprocess.STDOUT,
     )
     info("Setting up virtual environment")
     subprocess.run(
@@ -129,6 +133,8 @@ if USE_PIP:
         shell=True,
         check=True,
         cwd=SLASH_PATH,
+        stdout=subprocess.DEVNULL,
+        stderr=subprocess.STDOUT,
     )
 else:
     info("Running `poetry install`")
@@ -136,10 +142,12 @@ else:
         [found["poetry"], "install"],
         check=True,
         cwd=SLASH_PATH,
+        stdout=subprocess.DEVNULL,
+        stderr=subprocess.STDOUT,
     )
 
-# info("Generating post-selfhost instructions")
-print("\n\nAll set!\n")
+print("=" * shutil.get_terminal_size().columns)
+print("All set!\n")
 if found["psql"] is None:
     print("WARNING: PostgreSQL didn't seemed to be installed")
     print("Slashtilities can still function without PostgreSQL but")
@@ -149,12 +157,12 @@ print()
 if USE_PIP:
     print("    source .venv/bin/activate")
     print(
-        f"{'    NO_DB=1' if found['psql'] is None else '    '}DISCORD_TOKEN=<your discord token here> python3 -m slashtilities"
+        f"{'    NO_DB=1 ' if found['psql'] is None else '    '}DISCORD_TOKEN=<your discord token here> python3 -m slashtilities"
     )
 
 else:
     print(
-        f"{'    NO_DB=1' if found['psql'] is None else '    '}DISCORD_TOKEN=<your discord token here> poetry run python -m slashtilities"
+        f"{'    NO_DB=1 ' if found['psql'] is None else '    '}DISCORD_TOKEN=<your discord token here> poetry run python -m slashtilities"
     )
 
 print()
