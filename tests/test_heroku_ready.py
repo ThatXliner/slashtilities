e8174192e64c78201122c89e63ad58430dd282d9
ThatXliner <bryan.hu.2020@gmail.com>
2021-12-17 14:59:24 -0800

    Update test_heroku_ready.py

--- tests/test_heroku_ready.py
+++ tests/test_heroku_ready.py
@@ -3,6 +3,7 @@ from pathlib import Path
 
 
 def test_requirementstxt_is_up_to_date() -> None:
+    ...
     #assert (
     #    subprocess.check_output(["poetry", "export", "--without-hashes"]).decode().strip()
     #    == Path("requirements.txt").read_text().strip()
